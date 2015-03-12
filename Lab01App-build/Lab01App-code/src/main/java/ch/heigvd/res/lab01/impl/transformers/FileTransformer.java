d40f185729f5cc2814e04c9c90722d017cca53c1
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2015-03-12 18:10:41 +0100

    reintroduce encoding bug, so that I can give instructions to students

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/transformers/FileTransformer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/transformers/FileTransformer.java
@@ -50,7 +50,7 @@ public abstract class FileTransformer implements IFileVisitor {
     }
     try {
       Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8");
-      Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"), "UTF-8");
+      Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"));
       writer = decorateWithFilters(writer);
 
       /*
